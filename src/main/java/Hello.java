ecf7ef0e42286eb0e7803cf04c5fc850925c8ebb
aguttula705 <aguttula705@gmail.com>
2024-07-24 15:45:29 -0700

    Github hook test commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
    public static void main(String args[])
    {
-      System.out.println("Hello World - Poll Scm test commit");
+      System.out.println("Hello World - Github hook test commit");
    }
 }
